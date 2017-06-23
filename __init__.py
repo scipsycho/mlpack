60a383b667b6dabb9b46526a21df40aafbd78a0f
Dharmesh Singh <dsingh1664@outlook.com>
2017-06-23 16:14:08 +0530

    Updated __init__
      # Important Updates:
        -----------------
        . Updated Linear Regression
        . Added Logistic Regression
        . Added Package Documentation
        . Added Comments

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
-from mlpackages.linear_regression import linear_regression
+from mlpack.linear_regression import linear_regression
+from mlpack.logistic_regression import logistic_regression
+
+"""
+MlPack
+======
+
+Provides
+  1. A Variety of Machine learning packages
+  2. Good and Easy hand written programs with good documentation
+  3. Linear Regression, Logistic Regression
+
+ Available subpackages
+ ---------------------
+ 1. Linear Regression
+ 2. Logistic Regression
+
+ See subpackages for more details.
+"""
